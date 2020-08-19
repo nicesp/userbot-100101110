1faaf1fd34fe25287506ef57f4c0987ed54a7ec1
nicesp <69475238+nicesp@users.noreply.github.com>
2020-08-19 20:14:11 +0200

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -19,7 +19,7 @@ userbotversion = "2.1.0"
 os.system("pip install --upgrade pip")
 if Var.STRING_SESSION:
     session_name = str(Var.STRING_SESSION)
-    bot = TelegramClient(StringSession(session_name), Var.APP_ID, Var.API_HASH)
+    bot = TelegramClient(StringSession(session_name), Var.nicesplol27, Var. 635721377124f09fa69731f00789e883)
 else:
     session_name = "startup"
     bot = TelegramClient(session_name, Var.APP_ID, Var.API_HASH)
